954440030de53cabf9b0e8af8185be88b83416f6
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2024-07-22 18:28:23 +0530

    iam role changes

--- _outputs.tf
+++ _outputs.tf
@@ -19,10 +19,6 @@
 ##  This file contains code written by SevenPico, Inc.
 ## ----------------------------------------------------------------------------
 
-output "iam_role_arn" {
-  value = module.ssm_activation_iam_role.arn
-}
-
 output "expiration_date_RFC3339_format" {
   value = try(time_rotating.activation_code_rotation[0].id, "")
 }
--- examples/complete/iam.tf
+++ examples/complete/iam.tf
@@ -6,9 +6,19 @@ module "ssm_activation_iam_role" {
   version = "2.0.0"
   context = module.context.self
 
-  assume_role_actions      = ["sts:AssumeRole"]
-  assume_role_conditions   = []
-  instance_profile_enabled = true
+  assume_role_actions = ["sts:AssumeRole"]
+  assume_role_conditions = [
+    {
+      test     = "aws:SourceAccount"
+      variable = "StringEquals"
+      values   = [local.account_id]
+    },
+    {
+      test     = "aws:SourceArn"
+      variable = "StringEquals"
+      values   = ["${local.arn_prefix}:ssm:${local.region}:${local.account_id}:*"]
+  }]
+  instance_profile_enabled = false
   managed_policy_arns      = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
   max_session_duration     = 3600
   path                     = "/"
